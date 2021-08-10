d4729b261a7dfac205b3ac3745d5a4d036b18546
Administrator <admin@example.com>
2021-08-10 14:04:26 +0430

    Changed the urls to use github

--- vsdkx/cli/global_constants.py
+++ vsdkx/cli/global_constants.py
@@ -2,14 +2,14 @@ POSTFIX_MODEL = "-product"
 NAME_REGEX = "^([A-Za-z0-9\-.:])+$"
 
 manifest = {
-    "vsdkx-model-yolo-torch": "git+https://gitlab+deploy-token-488362:aQbEo4kAqEhppoUe3Tyh@gitlab.com/natix/cvison/vsdkx/vsdkx-model-yolo-torch",
-    "vsdkx-model-yolo-tflite": "git+https://gitlab+deploy-token-488360:bRkGozdPs1yUjjLQS4Zy@gitlab.com/natix/cvison/vsdkx/vsdkx-model-yolo-tflite",
-    "vsdkx-model-yolo-facemask": "git+https://gitlab+deploy-token-488359:fhs2HLs2ZHLxSvPHbt84@gitlab.com/natix/cvison/vsdkx/vsdkx-model-yolo-mask",
-    "vsdkx-model-bayesian": "git+https://gitlab+deploy-token-488354:wfv9ruzXiDj93Dw6wrmP@gitlab.com/natix/cvison/vsdkx/vsdkx-model-bayesian",
-    "vsdkx-model-resnet": "git+https://gitlab+deploy-token-488358:QVhHopKVuY4MWMyCNDEa@gitlab.com/natix/cvison/vsdkx/vsdkx-model-resnet",
-    "vsdkx-model-mobilenet": "git+https://gitlab+deploy-token-488355:bRf2y6xyhHjQZrUy_uzB@gitlab.com/natix/cvison/vsdkx/vsdkx-model-mobilenet",
-    "vsdkx-addon-distant": "git+https://gitlab+deploy-token-488346:fjijyZBz2gStuv46anZs@gitlab.com/natix/cvison/vsdkx/vsdkx-addon-distant",
-    "vsdkx-addon-facemask": "git+https://gitlab+deploy-token-488347:XxHx9VLvb7W4Ch1rnLt8@gitlab.com/natix/cvison/vsdkx/vsdkx-addon-facemask",
-    "vsdkx-addon-tracking": "git+https://gitlab+deploy-token-488350:1jt8j5EcWg5gfvRF4Bq1@gitlab.com/natix/cvison/vsdkx/vsdkx-addon-tracking",
-    "vsdkx-addon-zoning": "git+https://gitlab+deploy-token-488351:AaSywFKwJxPzv2qnFUia@gitlab.com/natix/cvison/vsdkx/vsdkx-addon-zoning"
+    "vsdkx-model-yolo-torch": "git+https://github.com/natix-io/vsdkx-model-yolo-torch.git",
+    "vsdkx-model-yolo-tflite": "git+https://github.com/natix-io/vsdkx-model-yolo-tflite.git",
+    "vsdkx-model-yolo-facemask": "git+https://github.com/natix-io/vsdkx-model-yolo-facemask.git",
+    "vsdkx-model-bayesian": "git+https://github.com/natix-io/vsdkx-model-bayesian.git",
+    "vsdkx-model-resnet": "git+https://github.com/natix-io/vsdkx-model-resnet.git",
+    "vsdkx-model-mobilenet": "git+https://github.com/natix-io/vsdkx-model-mobilenet.git",
+    "vsdkx-addon-distant": "git+https://github.com/natix-io/vsdkx-addon-distant.git",
+    "vsdkx-addon-facemask": "git+https://github.com/natix-io/vsdkx-addon-facemask.git",
+    "vsdkx-addon-tracking": "git+https://github.com/natix-io/vsdkx-addon-tracking.git",
+    "vsdkx-addon-zoning": "git+https://github.com/natix-io/vsdkx-addon-zoning.git"
 }
